04fe36f4cfc6afabe3f798d8ff5c1751658fc95e
Alex <53919963+tritonian@users.noreply.github.com>
2022-06-22 21:26:29 -0600

    Learned about component variables

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -1,22 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Projectile : MonoBehaviour
 {
     public Vector2 direction;
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        if (direction != null )
+        if (direction != null)
         {
             //move x direction
             
         }
+
     }
 
     public void SetSprite(Sprite newSprite)
     {
-        GetComponent<SpriteRenderer>().sprite = newSprite;
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
+        else
+        {
+            Debug.Log("Sprite renderer hasn't been set to a reference object!");
+        }
     }
 }

e088832aee31560f905e6c5eb1a888d25843a7e3
Alex <53919963+tritonian@users.noreply.github.com>
2022-06-24 15:50:29 -0600

    Projectile Movement
    Made projectiles move based on a given direction
    Gave them a circle collider slightly smaller than the sprite
    Also changed some folder structure

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour
+{
+    // settings
+    public Vector2 direction; // x, y vector that determines direction, for example (0, 1) up or (0, -1) down
+    public float stepLength; // scalar value to move each step
+
+    // local variables
+    private SpriteRenderer spriteRenderer; // the sprite renderer component on this game object - private because we get it in the start method
+
+    private void Start()
+    {
+        // gets the sprite renderer component on the gameobject this script is on
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (direction != null)
+        {
+            // if direction isn't empty, it has been assigned
+            // move some speed up/down based on that direction
+
+            Vector3 moveStep = stepLength * Time.deltaTime * direction; // the vector we want to move - scaled by deltaTime
+            transform.position = transform.position + moveStep; // add the vector we want to move to our current position
+        }
+    }
+
+    public void SetSprite(Sprite newSprite)
+    {
+        // check that we have a valid spriteRenderer assigned
+        if (spriteRenderer != null)
+        {
+            // set that sprite renderer to use the sprite that got passed in
+            spriteRenderer.sprite = newSprite;
+        }
+        else
+        {
+            Debug.Log("Sprite renderer hasn't been assigned!");
+        }
+    }
+}
